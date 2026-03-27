f559773ad33565189a97ec9a0ff2887ac436fa9c
TMinh-2512 <minhminh4282003@gmail.com>
2026-03-27 15:49:50 +0700

    Add Data Layer, migration

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string FullName { get; set; }
+
+        [Required, MaxLength(20)]
+        public string PhoneNumber { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Email { get; set; }
+
+        [MaxLength(50)]
+        public string Address { get; set; }
+    }
+}
---
+++ ContactManager/Models/ContactManagementDbContext.cs
@@ -0,0 +1,36 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ContactManagementDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=ContactDb;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact { Id = 1, FullName = "Nguyen Van An", PhoneNumber = "0912345678", Email = "nguyenvanan@example.com", Address = "Ha Noi" },
+                new Contact { Id = 2, FullName = "Tran Thi Binh", PhoneNumber = "0912345678", Email = "tranthibinh@example.com", Address = "Ho Chi Minh" },
+                new Contact { Id = 3, FullName = "Le Van Cuong", PhoneNumber = "0912345678", Email = "levancuong@example.com", Address = "Đa Nang" },
+                new Contact { Id = 4, FullName = "Pham Thi Dung", PhoneNumber = "0912345678", Email = "phamthidung@example.com", Address = "Hai Phong" },
+                new Contact { Id = 5, FullName = "Hoang Van Đuc", PhoneNumber = "0912345678", Email = "hoangvanduc@example.com", Address = "Can Tho" },
+                new Contact { Id = 6, FullName = "Đo Thi Hanh", PhoneNumber = "0912345678", Email = "dothihanh@example.com", Address = "Hue" },
+                new Contact { Id = 7, FullName = "Bui Van Giang", PhoneNumber = "0912345678", Email = "buivangiang@example.com", Address = "Nha Trang" }
+            );
+        }
+    }
+}
